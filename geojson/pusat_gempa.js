f58ed2712e9d6a0340692976ab5dd6d07e501974
ikhwanmuhammad13 <92528431+ikhwanmuhammad13@users.noreply.github.com>
2022-06-18 23:58:21 +0700

    Add files via upload

---
+++ geojson/pusat_gempa.js
@@ -0,0 +1,17 @@
+var titik_gempa =
+{
+"type": "FeatureCollection",
+"name": "pusat_gempa",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No": 1, "Tanggal": "2\/25\/2022", "Waktu": "08:35:51", "Y": 0.14, "X": 99.99, "Magnitudo": 5.2, "Kedalaman": "10 Km", "Wilayah": "18 km TimurLaut Pasaman Barat - Sumbar", "Time": "2022-02-25T08:35:51" }, "geometry": { "type": "Point", "coordinates": [ 99.99, 0.14 ] } },
+{ "type": "Feature", "properties": { "No": 2, "Tanggal": "2\/25\/2022", "Waktu": "08:39:29", "Y": 0.15, "X": 99.98, "Magnitudo": 6.2, "Kedalaman": "10 Km", "Wilayah": "17 km TimurLaut Ppasaman Barat - Sumbar", "Time": "2022-02-25T08:39:29" }, "geometry": { "type": "Point", "coordinates": [ 99.98, 0.15 ] } },
+{ "type": "Feature", "properties": { "No": 3, "Tanggal": "2\/25\/2022", "Waktu": "11:02:20", "Y": 0.15, "X": 99.97, "Magnitudo": 5.0, "Kedalaman": "10 Km", "Wilayah": "16 km TimurLaut Pasaman Barat - Sumbar", "Time": "2022-02-25T11:02:20" }, "geometry": { "type": "Point", "coordinates": [ 99.97, 0.15 ] } },
+{ "type": "Feature", "properties": { "No": 4, "Tanggal": "2\/25\/2022", "Waktu": "11:06:38", "Y": 0.12, "X": 99.98, "Magnitudo": 5.1, "Kedalaman": "10 Km", "Wilayah": "16 km TimurLaut Pasaman Barat - Sumbar", "Time": "2022-02-25T11:06:38" }, "geometry": { "type": "Point", "coordinates": [ 99.98, 0.12 ] } },
+{ "type": "Feature", "properties": { "No": 5, "Tanggal": "2\/25\/2022", "Waktu": "22:17:21", "Y": 0.16, "X": 100.01, "Magnitudo": 4.7, "Kedalaman": "8 Km", "Wilayah": "8 km Tenggara Talu, Pasaman Barat", "Time": "2022-02-25T22:17:21" }, "geometry": { "type": "Point", "coordinates": [ 100.01, 0.16 ] } },
+{ "type": "Feature", "properties": { "No": 6, "Tanggal": "2\/27\/2022", "Waktu": "02:08:18", "Y": 0.23, "X": 99.91, "Magnitudo": 4.9, "Kedalaman": "10 Km", "Wilayah": "7km BaratLaut Talu Pasaman Barat", "Time": "2022-02-27T02:08:18" }, "geometry": { "type": "Point", "coordinates": [ 99.91, 0.23 ] } },
+{ "type": "Feature", "properties": { "No": 7, "Tanggal": "3\/1\/2022", "Waktu": "22:37:03", "Y": 0.22, "X": 99.93, "Magnitudo": 4.5, "Kedalaman": "10 Km", "Wilayah": "5km BaratDaya Talu-Pasaman Barat", "Time": "2022-03-01T22:37:03" }, "geometry": { "type": "Point", "coordinates": [ 99.93, 0.22 ] } },
+{ "type": "Feature", "properties": { "No": 8, "Tanggal": "3\/2\/2022", "Waktu": "11:41:14", "Y": 0.26, "X": 99.89, "Magnitudo": 3.7, "Kedalaman": "3 Km", "Wilayah": "10 km barat laut Talu-Pasaman Barat", "Time": "2022-03-02T11:41:14" }, "geometry": { "type": "Point", "coordinates": [ 99.89, 0.26 ] } },
+{ "type": "Feature", "properties": { "No": 9, "Tanggal": "3\/3\/2022", "Waktu": "13:37:04", "Y": 0.15, "X": 100.0, "Magnitudo": 4.8, "Kedalaman": "10 Km", "Wilayah": "8 km tenggara Talu - Pasaman Barat", "Time": "2022-03-03T13:37:04" }, "geometry": { "type": "Point", "coordinates": [ 100.0, 0.15 ] } }
+]
+}
